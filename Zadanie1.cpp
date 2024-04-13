74512354b8f37db77c0e8ba3411fd4da6ce142e9
Andrey-78 <avk11121978@mail.ru>
2024-04-13 21:46:35 +0300

    Add new files

---
+++ Zadanie1.cpp
@@ -0,0 +1,29 @@
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x;
+    char ch;
+    float fl;
+    double dbl;
+    bool bl;
+    string str;
+    cout << "Enter Int\n";
+    cin >> x;
+    cout << "Enter Char\n";
+    cin >> ch;
+    cout << "Enter Float\n";
+    cin >> fl;
+    cout << "Enter Double\n";
+    cin >> dbl;
+    cout << "Enter Bool\n";
+    cin >> bl;
+    cout << "Enter Sting\n";
+    cin >> str;
+    cout << "\nYou entered:" << ' ' << x << "," << ' ' << ch << "," << ' ' << fl 
+        << "," << ' ' << dbl<< "," << ' ' << bl  << "," << ' ' << str << ".\n";
+}
+
+
